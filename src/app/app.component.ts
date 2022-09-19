600a783f668528103f371b9750b0517263c829a0
Karlz-Bandz <57764322+Karlz-Bandz@users.noreply.github.com>
2022-09-19 09:28:48 +0200

    initial commit - edit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular_hair_by_bieszczii_2_1';
+}
