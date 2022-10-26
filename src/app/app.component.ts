fa813bddeb548432ccd6b0fb7ba2a835843d18e0
Karlz-Bandz <57764322+Karlz-Bandz@users.noreply.github.com>
2022-10-26 10:22:48 +0200

    My Commit(x2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,23 @@ export class AppComponent {
   title = 'angular_hair_by_bieszczii_2_1';
 
 
+  @ViewChild("navscript") navscript: ElementRef | undefined;
 
-     
+     isMenuOpen = false;
+
+
+
+     public toggleMenu(): void {
+      //document.getElementById("navbar-script").style.display = "block";
+      this.isMenuOpen = true;
+
+    
+      
+     }
+
+     public hideMenu(): void{
+        this.isMenuOpen = false;
+    }
 
 
 
