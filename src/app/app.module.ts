24037378f99a132892dfbeef23679df5ca192319
Melak <km250542@ncr.com>
2023-04-26 18:55:16 +0200

    Animation success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -72,6 +73,7 @@ const routes: Routes = [{path: '', component: MainComponent},
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('popState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ])
+  ]
 })
 export class MainComponent implements OnInit {
 
   constructor() { }
 
+  show = false;
+
   ngOnInit(): void {
+
+    setTimeout(() => {
+      this.show = true;
+    }, 100);
+    
   }
+
+  get stateOfImg(){
+    return this.show ? 'show' : 'hide';
+  }
+
+  // toggle() {
+  //   this.show = !this.show;
+  // }
 
 }
