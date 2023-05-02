50862796dbad51ebebfe72b826c292dcb35193b7
Melak <km250542@ncr.com>
2023-05-02 14:42:25 +0200

    Mobile nav animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { SlideNav } from './slidenav';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [SlideNav]
 })
 export class AppComponent {
   title = 'angular_hair_by_bieszczii_2_1';
@@ -13,22 +15,34 @@ export class AppComponent {
 
   @ViewChild("navscript") navscript: ElementRef | undefined;
 
-     isMenuOpen = false;
+    //  isMenuOpen = false;
+
+     show = false;
+
+    
 
      constructor(private rout: Router) { }
 
+     get stateOfNav(){
+      return this.show ? 'show' : 'hide';
+    }
+
      public toggleMenu(): void {
       
-      this.isMenuOpen = true;
+      if(this.show)
+      {
+        // this.isMenuOpen = false;
+        this.show = false;
+        
+      }else{
+        // this.isMenuOpen = true;
+        this.show = true;
+        
+      }
+ }
 
     
-      
-     }
-
-     public hideMenu(): void{
-        this.isMenuOpen = false;
-    }
-    public goToLoginPage(): void{
+     public goToLoginPage(): void{
 
 
       if(localStorage.getItem("token") === null || localStorage.getItem("token") === "0"){
